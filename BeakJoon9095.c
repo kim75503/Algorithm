aeba296a5fa5afa192cb1404da45d7adb993d26e
kim75503 <84176988+kim75503@users.noreply.github.com>
2024-04-07 14:49:50 +0900

    Create BeakJoon9095.c

---
+++ BeakJoon9095.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n); 
+    int arr[11] = {0};
+
+    arr[1] = 1;
+    arr[2] = 2;
+    arr[3] = 4;
+
+    for (int i = 4; i < 11; i++) {
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int testNum;
+        scanf("%d", &testNum);
+        printf("%d\n", arr[testNum]);
+    }
+
+    return 0;
+}
