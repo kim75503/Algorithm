5e6c78ba7e5f44f8bdd0826c70dcb0cc7600130e
kim75503 <84176988+kim75503@users.noreply.github.com>
2024-05-09 15:12:37 +0900

    Create BeakJoon1004.c

---
+++ BeakJoon1004.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+typedef struct {
+    Point center;
+    int radius;
+} Circle;
+
+int containsPoint(Point p, Circle c) {
+    double distance = sqrt(pow(p.x - c.center.x, 2) + pow(p.y - c.center.y, 2));
+    return distance <= c.radius;
+}
+
+int countPointsInCircles(Point p1, Point p2, Circle* circles, int n) {
+    int count = 0;
+    
+    for (int i = 0; i < n; i++) {
+
+        int p1Inside = containsPoint(p1, circles[i]);
+        int p2Inside = containsPoint(p2, circles[i]);
+      
+        if (p1Inside && !p2Inside) {
+            count++;
+        } else if (!p1Inside && p2Inside) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+int main() {
+    int T;
+    scanf("%d", &T); 
+    
+    for (int t = 0; t < T; t++) {
+      
+        Point p1, p2;
+        scanf("%d %d %d %d", &p1.x, &p1.y, &p2.x, &p2.y);
+        
+       
+        int n;
+        scanf("%d", &n);
+    
+        Circle circles[n];
+        
+        for (int i = 0; i < n; i++) {
+           
+            Circle c;
+            scanf("%d %d %d", &c.center.x, &c.center.y, &c.radius);
+            circles[i] = c;
+        }
+        
+        int result = countPointsInCircles(p1, p2, circles, n);
+        
+        printf("%d\n", result);
+    }
+    
+    return 0;
+}
